2bd68472d7ed41f2e3527fc4d43dab2c19566a01
Adrian Messado <adrian.messado@gmail.com>
2018-12-16 15:37:15 -0800

    add additional tasks and regular item to list

--- todo.rb
+++ todo.rb
@@ -33,6 +33,16 @@ if __FILE__ == $PROGRAM_NAME
     my_list = List.new
     puts "You have created a new list"
     my_list.add(Task.new('Make Breakfast'))
+    my_list.add(Task.new('Study Ruby'))
+    my_list.add('Send bday card to Mom')
+
+    puts 'You have added a task to the Todo List'
+            if my_list.show.join.include?('#<')
+              print [
+                'Are you sure you are handling your task object correctly for showing',
+                "as a string?\n"
+                ]
+            end
     puts 'You have added a task to the Todo List'
     puts my_list.show
 end
