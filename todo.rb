1ca5568c9cf7705cb7eab70e10f4570a85fed1d6
Adrian Messado <adrian.messado@gmail.com>
2018-12-16 18:20:43 -0800

    comment out class Task, remove Task.new instantiation from my_list.add and test

--- todo.rb
+++ todo.rb
@@ -14,35 +14,29 @@ class List
 
     #method displays all tasks
     def show
-        all_tasks
+        @all_tasks
+        # .select { |i| puts i.include?()}
     end
 
 end
 
 #add tasks to list - manages actions that can be implemented on a task object
-class Task
-    attr_reader :description
+# class Task
+#     attr_reader :description
 
-    def initialize(description)
-        @description = description
-    end
-end
+#     def initialize(description)
+#         @description = description
+#     end
+
+# end
 
 #Program runner
 if __FILE__ == $PROGRAM_NAME
     my_list = List.new
     puts "You have created a new list"
-    my_list.add(Task.new('Make Breakfast'))
-    my_list.add(Task.new('Study Ruby'))
+    my_list.add('Make Breakfast')
+    my_list.add('Study Ruby')
     my_list.add('Send bday card to Mom')
-
-    puts 'You have added a task to the Todo List'
-            if my_list.show.join.include?('#<')
-              print [
-                'Are you sure you are handling your task object correctly for showing',
-                "as a string?\n"
-                ]
-            end
     puts 'You have added a task to the Todo List'
     puts my_list.show
 end
