29fddf05e3463649dc9df369f44fc448226e4817
Adrian Messado <adrian.messado@gmail.com>
2018-12-18 13:02:16 -0800

    add until statement that sets user input equal to user_input using the Promptable modules show method until users presses Q or q

--- todo.rb
+++ todo.rb
@@ -1,64 +1,69 @@
 module Menu
-    def menu
-        puts "Welcome the ToDo List App.
-                Here are a list of options:
-                1) Add
-                2) Show
-                Q) Quit "
-    end
+	def menu
+		puts "Welcome the ToDo List App.
+				Here are a list of options:
+				1) Add
+				2) Show
+				Q) Quit "
+	end
 
-    def show
-        menu
-    end
+	def show
+		menu
+	end
 end
 
 module Promptable
-    def prompt(message = 'What would you like to do?', symbol = ':> ')
-        print message
-        print symbol
-        gets.chomp
-    end
+	def prompt(message = 'What would you like to do?', symbol = ':> ')
+		print message
+		print symbol
+		gets.chomp
+	end
 end
 
 
 #create a Todo List -  the class List handles actions implemented on a List object.
 #manages the behavior of an individual list
 class List
-    attr_reader :all_tasks
+	attr_reader :all_tasks
 
 
-    def initialize
-        @all_tasks = []
-    end
+	def initialize
+		@all_tasks = []
+	end
 
-    #method adds task to all_tasks array
-    def add(task)
-        @all_tasks << task
-    end
+	#method adds task to all_tasks array
+	def add(task)
+		@all_tasks << task
+	end
 
-    #method displays all tasks
-    def show
-        @all_tasks
-        # .select { |i| puts i.include?()}
-    end
+	#method displays all tasks
+	def show
+		@all_tasks
+		# .select { |i| puts i.include?()}
+	end
 
 end
 
 #add tasks to list - manages actions that can be implemented on a task object
 #manages the behavior of an individual task.
 class Task
-    attr_reader :description
+	attr_reader :description
 
-    def initialize(description)
-        @description = description
-    end
+	def initialize(description)
+		@description = description
+	end
 
 end
 
 #Program runner
 if __FILE__ == $PROGRAM_NAME
-        include Menu
-        include Promptable
-        my_list = List.new
-
+	my_list = List.new
+	include Menu
+	include Promptable
+	# puts menu
+	# puts prompt
+	#Until the user quits, display the menu and prompt the user for their input
+	until 'q'.include?(user_input = prompt(*show).downcase)
+		puts user_input
+	end
 end
