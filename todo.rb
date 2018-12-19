eb45057124f4a806e7583a6e4008b8566a9babd3
Adrian Messado <adrian.messado@gmail.com>
2018-12-18 22:38:05 -0800

    add delete option for #3 on case statement in  program runner

--- todo.rb
+++ todo.rb
@@ -86,14 +86,16 @@ if __FILE__ == $PROGRAM_NAME
 		case user_input
 			when "1" then my_list.add(prompt('What is it you would like to add?'))
 			when "2" then puts my_list.show
-			when "3" my_list.delete
+			when "3"
+				puts my_list.show
+				my_list.delete(prompt("Enter number of item to delete").to_i)
 			when "4" then my_list.write_to_file(prompt("Please enter filename"))
 			when '5'
 				begin
 					 my_list.read_from_file(prompt("Please enter filename"))
-				rescue Errno::ENOENT
-					puts 'File name not found, please verify your file name
-				  and path.'
+				# rescue Errno::ENOENT
+				# 	puts 'File name not found, please verify your file name
+				#   and path.'
 				end
 			else puts "I don't understand that response"
 		end
